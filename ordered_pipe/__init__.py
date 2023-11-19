238409cfaa56d5fe43b96d098b96873259382745
nutax <jose.huby@utec.edu.pe>
2023-11-19 13:57:19 -0500

    owo

--- ordered_pipe/__init__.py
+++ ordered_pipe/__init__.py
@@ -1,46 +1,35 @@
 import concurrent.futures
 
-
-
 class OrderedPipe:
     def __init__(self, **kwargs):
         self.inputs = kwargs
         self.task_groups = OrderedPipe.parallel_order(kwargs)
 
     def parallel_order(dependency_graph):
-        # Create a dictionary mapping nodes to their in-degrees and dependency set
         in_degree = {node: 0 for node in dependency_graph}
         dependencies = {node: set() for node in dependency_graph}
 
-        # Populate the in-degree and dependencies
         for node, (operation, *operands) in dependency_graph.items():
             for operand in operands:
-                if operand in dependency_graph:  # Only consider internal nodes
+                if operand in dependency_graph:
                     in_degree[node] += 1
                     dependencies[operand].add(node)
 
-        # List to hold the result as a series of steps with parallelizable tasks
         parallel_steps = []
 
-        # Find all nodes with an in-degree of 0 (i.e., no dependencies)
         independent_nodes = [node for node, degree in in_degree.items() if degree == 0]
 
-        # While there are independent nodes, process them
         while independent_nodes:
-            # Add to the current step
             parallel_steps.append(independent_nodes)
 
-            # Nodes to check for the next step
             next_step = []
 
-            # For each independent node, reduce the in-degree of its dependent nodes
             for node in independent_nodes:
                 for dependent in dependencies[node]:
                     in_degree[dependent] -= 1
                     if in_degree[dependent] == 0:
                         next_step.append(dependent)
 
-            # The next step now becomes the independent nodes
             independent_nodes = next_step
 
         return parallel_steps
